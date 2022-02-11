4f759e6377599c1926cedab8354ad166b5f064e1
backas36 <backas36@gmail.com>
2022-02-11 15:37:49 +0800

    learn how to upload file in express and axios ajax

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require("express");
-//const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const app = express();
+const multer = require("multer");
+const fs = require("fs");
+
+//上傳檔案路徑
+const uploadConfig = multer({
+  dest: "./upload",
+});
+
+//使用傳統方式實現檔案上傳
+app.post("/api/upload", uploadConfig.any(), (req, res) => {
+  console.log(req.files);
+  for (let i = 0; i < req.files.length; i++) {
+    let file = req.files[i];
+    fs.renameSync(file.path, "./upload/" + file.originalname);
+  }
+  res.write("<h1>yes</h1>");
+  res.end();
+});
 
 app.use(express.json());
-//app.use(express.urlencoded({ extends: false }));
+app.use(express.urlencoded({ extends: false })); //這樣才抓得到 formData
 app.use(express.static("./public"));
 app.get("/api/get", (req, res) => {
   console.log(req.query);
