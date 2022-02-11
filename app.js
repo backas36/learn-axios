6a246ed74dee57830e1ba36be626d23d328b2115
backas36 <backas36@gmail.com>
2022-02-11 15:17:13 +0800

    axios post and install multer

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+//const bodyParser = require("body-parser");
 const app = express();
+
+app.use(express.json());
+//app.use(express.urlencoded({ extends: false }));
 app.use(express.static("./public"));
 app.get("/api/get", (req, res) => {
   console.log(req.query);
   res.json({ name: "Ashi" });
+});
+
+app.post("/api/post", (req, res) => {
+  console.log("/api/post =>", req.body);
+  res.end();
 });
 
 app.listen(5000);
